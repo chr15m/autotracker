ef3943e0b2d78550ff4cb75aeaf4b4988e81841d
Chris McCormick <chris@mccormick.cx>
2016-01-13 22:23:15 +0800

    Got breakbeat generation working first pass.

--- autotracker-breaks-chipsynths.py
+++ autotracker-breaks-chipsynths.py
@@ -6,7 +6,7 @@ import sys
 import random
 
 from strategies import Strategy_Main
-from generators import Generator_Drums, Generator_Bass, Generator_AmbientMelody
+from generators import Generator_Breaks, Generator_Bass, Generator_AmbientMelody
 from keys import Key_Minor, Key_Major
 from it.sample import Sample_KS, Sample_Kicker, Sample_NoiseHit, Sample_File, Sample_FileSlice
 from it.itfile import ITFile
@@ -25,30 +25,25 @@ if len(sys.argv) > 1:
 print "Creating module"
 itf = ITFile()
 
+pace = random.random()
 # set the tempo
-itf.tempo = random.randint(120,185)
+itf.tempo = int(pace * 65 + 120)
 
 # path to this script's home
 home = os.path.dirname(__file__)
 samples = os.path.join(home, "samples")
 
 print "Generating samples"
-# these could do with some work, they're a bit crap ATM --GM
-# note: commented a couple out as they use a fair whack of space and are unused.
 MIDDLE_C = 220.0 * (2.0 ** (3.0 / 12.0))
-#SMP_PIANO = itf.smp_add(Sample_KS(name = "KS Piano", freq = MIDDLE_C, decay = 0.07, nfrqmul = 0.02, filtdc = 0.1, filt0 = 0.09, filtn = 0.6, filtf = 0.4, length_sec = 1.0))
-#SMP_HOOVER = itf.smp_add(Sample_Hoover(name = "Hoover", freq = MIDDLE_C))
 
 def brk():
-    return samples + "/" + random.choice(["think.wav", "amen.wav"])
+    return samples + "/" + random.choice(["amen.wav", "think.wav"])
 
 def bleep(t):
     return samples + "/" + random.choice([f for f in os.listdir(samples) if f.startswith("c64") and "-" + t + ".wav" in f])
 
-SMP_KICK = itf.smp_add(Sample_FileSlice(filename = brk(), which=0))
-SMP_HHC = itf.smp_add(Sample_FileSlice(filename = brk(), which=5))
-SMP_HHO = itf.smp_add(Sample_FileSlice(filename = brk(), which=5))
-SMP_SNARE = itf.smp_add(Sample_FileSlice(filename = brk(), which=4))
+which_break = brk()
+SMP_BREAK = [itf.smp_add(s) for s in [Sample_FileSlice(filename=which_break, which=x) for x in xrange(8)]]
 
 SMP_HI = itf.smp_add(Sample_File(name = "bleep", filename=bleep("hi")))
 
@@ -59,7 +54,7 @@ else:
 
 print "Generating patterns"
 strat = Strategy_Main(random.randint(50,50+12-1)+12, Key_Minor if random.random() < 0.6 else Key_Major, 128, 32)
-strat.gen_add(Generator_Drums(s_kick = SMP_KICK, s_snare = SMP_SNARE, s_hhc = SMP_HHC, s_hho = SMP_HHO))
+strat.gen_add(Generator_Breaks(s_chunks=SMP_BREAK, pitch=int(pace * 12 + 54)))
 strat.gen_add(Generator_AmbientMelody(smp = SMP_HI))
 strat.gen_add(Generator_Bass(smp = SMP_BASS))
 
