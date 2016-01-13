042f5ebdc5d5cc7b3202cd939c66399ce79a1d70
Chris McCormick <chris@mccormick.cx>
2016-01-13 16:05:50 +0800

    Fixed relative path invocation of the script to find the samples dir correctly.

--- autotracker-chipsynths.py
+++ autotracker-chipsynths.py
@@ -25,6 +25,10 @@ if len(sys.argv) > 1:
 print "Creating module"
 itf = ITFile()
 
+# path to this script's home
+home = os.path.dirname(__file__)
+samples = os.path.join(home, "samples")
+
 # TODO: Generator_ArpMadness
 # TODO: Generator_FractalAcid
 # TODO: Generator_DrumLoops
@@ -41,12 +45,12 @@ SMP_HHC = itf.smp_add(Sample_NoiseHit(name = "NH Hihat Closed", gvol = 32, decay
 SMP_HHO = itf.smp_add(Sample_NoiseHit(name = "NH Hihat Open", gvol = 32, decay = 0.5, filtl = 0.99, filth = 0.20))
 SMP_SNARE = itf.smp_add(Sample_NoiseHit(name = "NH Snare", decay = 0.12, filtl = 0.15, filth = 0.149))
 
-SMP_HI = itf.smp_add(Sample_File(name = "bleep", filename="samples/" + random.choice([f for f in os.listdir("samples") if f.startswith("c64") and "-hi." in f])))
+SMP_HI = itf.smp_add(Sample_File(name = "bleep", filename=samples + "/" + random.choice([f for f in os.listdir(samples) if f.startswith("c64") and "-hi." in f])))
 
 if random.randint(0, 1):
     SMP_BASS = itf.smp_add(Sample_KS(name = "KS Bass", freq = MIDDLE_C/4, decay = 0.005, nfrqmul = 0.5, filt0 = 0.2, filtn = 0.2, filtf = 0.005, length_sec = 0.7))
 else:
-    SMP_BASS = itf.smp_add(Sample_File(name = "bleep-bass", filename="samples/" + random.choice([f for f in os.listdir("samples") if f.startswith("c64") and "-lo." in f])))
+    SMP_BASS = itf.smp_add(Sample_File(name = "bleep-bass", filename=samples + "/" + random.choice([f for f in os.listdir(samples) if f.startswith("c64") and "-lo." in f])))
 
 print "Generating patterns"
 strat = Strategy_Main(random.randint(50,50+12-1)+12, Key_Minor if random.random() < 0.6 else Key_Major, 128, 32)
